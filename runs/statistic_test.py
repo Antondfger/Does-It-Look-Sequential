68f82910a7f8e1cad5531be3a0f9b93ca31e27f4
Anton <apembek@bk.ru>
2024-07-09 14:45:32 +0000

    add statistics

--- runs/statistic_test.py
+++ runs/statistic_test.py
@@ -26,15 +26,16 @@ def main(config):
     else:
         task = None
 
-    stats = stat_diff(data_folder=config.datasets_info.path_to_metrics_by_user, dataset=config.datasets_info.name, 
-                      metric_name=config.metric_name, alpha=config.alpha, n_iter=config.n_iter, seed=config.seed)
+    result, mean = stat_diff(data_folder=config.datasets_info.path_to_metrics_by_user, dataset=config.datasets_info.name, 
+                             metric_name=config.metric_name, alpha=config.alpha, n_iter=config.n_iter, seed=config.seed)
 
-    print(stats)
+    print(result, mean)
 
     if task:
         
         clearml_logger = task.get_logger()
-        clearml_logger.report_single_value('stat_diff', stats)
+        clearml_logger.report_single_value('stat_diff', result)
+        clearml_logger.report_single_value('mean', mean)
 
 if __name__ == "__main__":
 
