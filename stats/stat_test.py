d3107179135afe9aef800b110802a0c24ea3cd56
Anton <apembek@bk.ru>
2024-07-10 14:18:59 +0000

    add notebooks

--- stats/stat_test.py
+++ stats/stat_test.py
@@ -10,9 +10,12 @@ def check_hyp_equality(df_pilot_group, df_control_group, metric_name, alpha=0.05
     bootstrap_control = np.random.choice(df_control_group[metric_name], size=(len(df_control_group), n_iter))
     
     bootstrap = bootstrap_pilot.mean(1) - bootstrap_control.mean(1)
+
     mean = float(df_pilot_group.mean()- df_control_group.mean())
+    res = mean+float(norm.ppf(alpha/2)*bootstrap.std())<=0<=mean+float(norm.ppf(1-alpha/2)*bootstrap.std())
     
-    return mean+float(norm.ppf(alpha/2)*bootstrap.std())<=0<=mean+float(norm.ppf(1-alpha/2)*bootstrap.std())
+    return res, mean
+
 
 def stat_diff(data_folder, dataset, metric_name, alpha=0.05, n_iter=1000, seed=None):
     """Shows statistical differences between user metrics for shuffle and non-shuffle data.
@@ -26,10 +29,10 @@ def stat_diff(data_folder, dataset, metric_name, alpha=0.05, n_iter=1000, seed=N
         seed (int): Defaults to None.
        
     Returns:
-        True if there are no statistically significant differences, false if there are.
+        True if there are no statistically significant differences, false if there are and confidence interval.
     """
-    shuffle = pd.read_csv(f"{data_folder}/{metric_name}_{dataset}_shuffle.csv",index_col="Unnamed: 0")
-    origin = pd.read_csv(f"{data_folder}/{metric_name}_{dataset}_origin.csv",index_col="Unnamed: 0")
-    res=check_hyp_equality(origin,shuffle,metric_name,n_iter=n_iter,seed=seed,alpha=alpha)
+    shuffle = pd.read_csv(f"{data_folder}/{metric_name}_{dataset}_shuffle.csv")
+    origin = pd.read_csv(f"{data_folder}/{metric_name}_{dataset}_origin.csv")
+    res, mean = check_hyp_equality(origin,shuffle,metric_name,n_iter=n_iter,seed=seed,alpha=alpha)
 
-    return res
+    return res, mean
